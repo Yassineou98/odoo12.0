7bcaecef237f19dbea8878ff2e53ff5c40067f61
Yassineou98 <57841155+Yassineou98@users.noreply.github.com>
2020-04-26 14:04:17 +0100

    clash free time table

---
+++ helpdesk_lite/models/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from . import res_users
+from . import helpdesk_stage
+from . import helpdesk_team
+from . import helpdesk_ticket
+from . import helpdesk_type_stage
+from . import helpdesk_motif
+from . import helpdesk_departement
+from . import helpdesk_service
+# from . import res_config
+
